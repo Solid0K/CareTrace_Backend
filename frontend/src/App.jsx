df481a189e7f778524fcca51440908c5c1707167
Kartik Upadhyay <kartikupadhyay200@gmail.com>
2026-09-11 00:59:41 +0530

    Patient Navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Patientheader from "./components/patient/Patientheader";
+function App() {
+  return (
+    <div className="App">
+      <Patientheader />
+    </div>
+  );
+}
+export default App;
