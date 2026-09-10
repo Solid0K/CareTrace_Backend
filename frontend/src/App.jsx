2e2ee811c295980453198b017a1dd4055faca9d2
Kartik Upadhyay <kartikupadhyay200@gmail.com>
2026-09-11 03:33:20 +0530

    Home Page designed routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Patientheader from "./components/patient/Patientheader";
+import PatientHome from "./pages/patient/PatientHome";
+import Medicines from "./pages/patient/Medicines";
+import Appointments from "./pages/patient/Appointments";
+import Todos from "./pages/patient/Todos";
+import Activities from "./pages/patient/Activities";
+
 function App() {
   return (
-    <div className="App">
-      <Patientheader />
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<PatientHome />} />
+
+        <Route path="/medicines" element={<Medicines />} />
+
+        <Route path="/appointments" element={<Appointments />} />
+
+        <Route path="/todos" element={<Todos />} />
+
+        <Route path="/activities" element={<Activities />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
